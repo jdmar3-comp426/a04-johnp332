f72e0c6df400938bafd3383e688ea65ef7212358
johnp332 <johnp332@live.unc.edu>
2021-11-28 18:37:56 -0500

    what's the problem

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Define app using express
-var express = require("express");
+var express = require("express")
 var app = express()
 // Require database SCRIPT file
 var db = require("./database.js");
@@ -11,10 +11,9 @@ app.use(express.json());
 
 // Set server port
 var HTTP_PORT = 5000;
-
 // Start server
 app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+    //console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 
 // READ (HTTP method GET) at root endpoint /app/
@@ -22,6 +21,8 @@ app.get("/app/", (req, res, next) => {
     res.json({"message":"Your API works! (200)"});
 	res.status(200);
 });
+
+// Define other CRUD API endpoints using express.js and better-sqlite3
 
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new", (req, res) => {
@@ -61,4 +62,3 @@ app.use(function(req, res){
     res.json({"message":"Endpoint not found. (404)"});
     res.status(404);
 });
-
